2b375abff01438f0c2b20b476792582de8078c6d
ToolmanP <ToolmanP@outlook.com>
2021-09-18 00:50:22 +0800

    Moved consts.py

---
+++ course_plus_data_fetcher/fetchers/consts.py
@@ -0,0 +1,5 @@
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44',
+    'Referer': 'https://i.sjtu.edu.cn/design/viewFunc_cxDesignFuncPageIndex.html?gnmkdm=N219904&layout=default'
+}
+TRIMESTER_MAP = {1: 3, 2: 12, 3: 16}
