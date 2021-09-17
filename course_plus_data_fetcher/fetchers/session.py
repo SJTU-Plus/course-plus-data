ea248b0a7ca701b29d3b090b1cd355aacd936b3f
ToolmanP <ToolmanP@outlook.com>
2021-09-18 00:52:12 +0800

    Moved session.py

---
+++ course_plus_data_fetcher/fetchers/session.py
@@ -0,0 +1,16 @@
+from httpx import AsyncClient
+from pysjtu import Session as SJTUSession
+
+from .consts import HEADERS
+
+
+class FetchSession:
+    def __init__(self, username: str, password: str):
+        self._account_session = SJTUSession(username, password)
+        self._fetch_session = AsyncClient(cookies=self._account_session.cookies, headers=HEADERS)
+
+    async def get(self, url: str, params: dict = None, timeout: int = 5, **kwarg):
+        return await self._fetch_session.post(url, params=params, timeout=timeout, **kwarg)
+
+    async def post(self, url: str, params: dict = None, data: dict = None, timeout: int = 5, **kwarg):
+        return await self._fetch_session.post(url, params=params, data=data, timeout=timeout, **kwarg)
